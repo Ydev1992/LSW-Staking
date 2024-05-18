f7699ceb063dcf2920cf931eeabbb05d9838c76e
johnsmith03070209 <js2035464@gmail.com>
2024-05-19 03:05:30 +0900

    final issue resolved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,50 +59,17 @@ const Header = () => {
       className={`fixed left-0 top-0 z-99999 w-full py-4 bg-primary shadow-lg transition duration-100 rounded-sm`}
     >
       <div className="relative flex mx-auto max-w-full items-center justify-between px-7 sm:px-8  2xl:px-20">
-        <div className="flex w-full items-center justify-between">
+        <div className="flex items-center justify-between">
           <Logo />
-          <button aria-label="hamburger Toggler" className="block sm:hidden">
-            <span className="relative block h-5.5 w-5.5 cursor-pointer">
-              <span className="absolute right-0 block h-full w-full">
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !navigationOpen ? "!w-full delay-300" : "w-0"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !navigationOpen ? "delay-400 !w-full" : "w-0"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !navigationOpen ? "!w-full delay-500" : "w-0"
-                  }`}
-                ></span>
-              </span>
-              <span className="du-block absolute right-0 h-full w-full rotate-45">
-                <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !navigationOpen ? "!h-0 delay-[0]" : "h-full"
-                  }`}
-                ></span>
-                <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                    !navigationOpen ? "!h-0 delay-200" : "h-0.5"
-                  }`}
-                ></span>
-              </span>
-            </span>
-          </button>
         </div>
 
         <div
-          className={`hidden items-center gap-8 md:visible sm:flex sm:h-auto sm:w-full content-end ${
+          className={` items-center gap-8 sm:flex sm:h-auto  ${
             navigationOpen &&
             "navbar !visible sm:mt-0 h-auto max-h-[400px] rounded-md bg-white p-7.5 shadow-solid-5 dark:bg-blacksection xl:h-auto xl:p-0 xl:shadow-none xl:dark:bg-transparent"
           }`}
         >
-          <nav>
+          <nav className="md:block  hidden">
             <ul className="flex gap-5 flex-row xl:items-center xl:gap-10">
               {menuData.map((menuItem, key) => (
                 <li key={key} className={menuItem.submenu && "group relative"}>
@@ -117,13 +84,12 @@ const Header = () => {
             </ul>
           </nav>
 
-          <div className="mt-7 invisible flex items-center gap-6 lg:mr-19 xl:mt-0 lg:visible">
+          <div className="flex items-center gap-6 xl:mt-0">
             <Image src={Twitter} className="!w-[34px] !h-[34px]" />
             <Image src={Telegram} className="!w-[34px] !h-[34px]" />
             <Image src={Discord} className="!w-[34px] !h-[34px]" />
           </div>
         </div>
-        <w3m-button />
       </div>
     </header>
   );
